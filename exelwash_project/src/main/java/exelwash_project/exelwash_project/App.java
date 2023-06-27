962d41705d44eac4783480387eca0a2556394f9f
ABHINAV <ABHINAV@LAPTOP-Q6BA1GAL>
2023-06-27 11:56:49 +0530

    ExelWash

---
+++ exelwash_project/src/main/java/exelwash_project/exelwash_project/App.java
@@ -0,0 +1,13 @@
+package exelwash_project.exelwash_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
